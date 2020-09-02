8afce67ec537768b5d7cf1f1c4e62ef937bcefb7
cs60181888 <qwerkdud1999@gmail.com>
2020-09-02 12:45:32 +0900

    object 사용 예제

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function readExcel() {
     let input = event.target;
     let reader = new FileReader();
+    var index = 1;
     reader.onload = function () {
         let data = reader.result;
         let workBook = XLSX.read(data, { type: 'binary' });
@@ -8,7 +9,7 @@ function readExcel() {
             console.log('SheetName: ' + sheetName);
             let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
             console.log(JSON.stringify(rows));
-            console.log(rows[1]);
+            console.log(rows[index]['출발역']);
         })
     };
     reader.readAsBinaryString(input.files[0]);
