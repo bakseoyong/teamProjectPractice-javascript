9af241d3b41fc0905492e1f422d11a67ba83e913
cs60181888 <qwerkdud1999@gmail.com>
2020-09-02 12:33:20 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function readExcel() {
+    let input = event.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        let data = reader.result;
+        let workBook = XLSX.read(data, { type: 'binary' });
+        workBook.SheetNames.forEach(function (sheetName) {
+            console.log('SheetName: ' + sheetName);
+            let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
+            console.log(JSON.stringify(rows));
+            console.log(rows[1]);
+        })
+    };
+    reader.readAsBinaryString(input.files[0]);
+}
