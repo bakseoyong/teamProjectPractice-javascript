f711073d2ee42cf89eb9e6c15d2433cd1278dc9d
cs60181888 <qwerkdud1999@gmail.com>
2020-09-26 14:24:53 +0900

    Menubar 컴포넌트로 떼어내기 test 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
+import Menubar from './Components/Menubar';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {
-    
+    menubarActive: false,
   };
 
-  iconStyle = {
+  iconStyle45px = {
     fontSize: "45px"
+  }
+
+  iconStyle50px = {
+    fontSize: "50px"
+  }
+
+  getMenubarComponent(){
+    var _a = null;
+    if(this.state.menubarActive === true){
+      _a = <Menubar></Menubar>
+    }
+    return _a;
   }
 
   render(){
@@ -16,19 +29,23 @@ class App extends Component {
       <div className="App">
         <div className="menubar">
           <div className="menubar_icon">
-              <i className="fas fa-home" style={this.iconStyle}></i>
-              <i className="fas fa-map-marked-alt" style={this.iconStyle}></i>
-              <i className="fas fa-directions" style={this.iconStyle}></i>
-              <i className="fas fa-subway" style={this.iconStyle}></i>
+              <i className="fas fa-home" style={this.iconStyle45px}></i>
+              <i className="fas fa-map-marked-alt" style={this.iconStyle45px}></i>
+              <i className="fas fa-directions" style={this.iconStyle50px}></i>
+              <i className="fas fa-subway" style={this.iconStyle50px}></i>
           </div>
         </div>
-
-        <div class="inteface">
-          <div class="menubar_active_button">
-            <i class="fas fa-angle-right"></i>
+        {this.getMenubarComponent()}
+        <div className="map">
+          <div className="menubar_active_button">
+            <i className="fas fa-angle-right" onClick={()=>{
+              this.setState({
+                menubarActive : true
+              })
+            }}></i>
           </div>
 
-          <img src="./subway_img.PNG" alt="subway" />
+          {/* <img src="./subway_img.PNG" alt="subway" /> */}
         </div>
       </div>
     );
