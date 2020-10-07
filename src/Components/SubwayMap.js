cd3c681bfc24e7745e4468cc10060909d5d89caa
cs60181888 <qwerkdud1999@gmail.com>
2020-10-07 21:11:24 +0900

    노선도 버튼 동작이 if문에서 템플릿리터럴 이용으로 변경. 코드 심플해짐.

--- src/Components/SubwayMap.js
+++ src/Components/SubwayMap.js
@@ -2,35 +2,12 @@ import React, {Component} from 'react';
 
 class SubwayMap extends Component{
     render(){
-
-        const sectionStyle = {
-            width: "100%",
-            height: "400px",
-            backgroundImage: "/images/subway_img_" + 0 + ".png"
+        const subwayImgPath = {
+            backgroundImage: `/images/subway_img_${this.props.lineNum}.png`
         }
 
         const setSubwayImg = (lineNum) => {  
-            if(lineNum === 0){
-                return <img src={sectionStyle.backgroundImage} alt="지도" id="subway_img"/>                
-            }else if(lineNum === 1){
-                return <img src='/images/subway_img_1.png' alt="1호선지도" id="subway_img_1"/>
-            }else if(lineNum === 2){
-                return <img src='/images/subway_img_2.PNG' alt="2호선지도" id="subway_img_2"/>
-            }else if(lineNum === 3){
-                return <img src='/images/subway_img_3.png' alt="3호선지도" id="subway_img_3"/>
-            }else if(lineNum === 4){
-                return <img src='/images/subway_img_4.png' alt="4호선지도" id="subway_img_4"/>
-            }else if(lineNum === 5){
-                return <img src='/images/subway_img_5.png' alt="5호선지도" id="subway_img_5"/>
-            }else if(lineNum === 6){
-                return <img src='/images/subway_img_6.png' alt="6호선지도" id="subway_img_6"/>
-            }else if(lineNum === 7){
-                return <img src='/images/subway_img_7.png' alt="7호선지도" id="subway_img_7"/>
-            }else if(lineNum === 8){
-                return <img src='/images/subway_img_8.png' alt="8호선지도" id="subway_img_8"/>
-            }else if(lineNum === 9){
-                return <img src='/images/subway_img_9.png' alt="9호선지도" id="subway_img_9"/>
-            }
+            return <img src={subwayImgPath.backgroundImage} alt="지도" id="subway_img"/>                
         }
 
         return(    
